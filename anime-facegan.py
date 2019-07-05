1c655b9b3a2950615d029c24972fc97ec14eb490
Nikita Sharma <36643628+nikitaa30@users.noreply.github.com>
2019-07-05 16:48:18 +0530

    Update anime-facegan.py

--- anime-facegan.py
+++ anime-facegan.py
@@ -32,35 +32,18 @@ import keras.backend as K
 from keras.initializers import RandomNormal
 K.set_image_dim_ordering('tf')
 
-import os
 import glob
 os.environ["KERAS_BACKEND"] = "tensorflow"
-import numpy as np
 from sklearn.utils import shuffle
-import time
-import cv2
 import scipy
 import imageio
 from PIL import Image
 import matplotlib.gridspec as gridspec
-from keras.layers import Dense
-from keras.layers import Reshape
-from keras.layers.core import Activation
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.core import Flatten
-from keras.layers import Input
-from keras.layers import Conv2D, Conv2DTranspose, Dropout
-from keras.models import Model
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.layers.advanced_activations import LeakyReLU
-# from GAN_Nets import get_disc_normal, get_gen_normal
 import matplotlib.pyplot as plt
 from keras.models import load_model
 import keras.backend as K
 from scipy.interpolate import spline
-K.set_image_dim_ordering('tf')
+
 
 from collections import deque
 
@@ -158,9 +141,11 @@ np.random.seed(1337)
 
 def norm_img(img):
     img = (img / 127.5) - 1
+    #image normalisation to keep values between -1 and 1 for stability
     return img
 
 def denorm_img(img):
+    #for output
     img = (img + 1) * 127.5
     return img.astype(np.uint8) 
 
@@ -172,12 +157,8 @@ def sample_from_dataset(batch_size, image_shape, data_dir=None, data = None):
     sample_imgs_paths = np.random.choice(all_data_dirlist,batch_size)
     for index,img_filename in enumerate(sample_imgs_paths):
         image = Image.open(img_filename)
-        #print(image.size)
-        #image.thumbnail(image_shape[:-1], Image.ANTIALIAS) - this maintains aspect ratio ; we dont want that - we need m x m size
         image = image.resize(image_shape[:-1])
-        image = image.convert('RGB') #remove transparent ('A') layer
-        #print(image.size)
-        #print('\n')
+        image = image.convert('RGB')
         image = np.asarray(image)
         image = norm_img(image)
         sample[index,...] = image
@@ -188,15 +169,13 @@ def sample_from_dataset(batch_size, image_shape, data_dir=None, data = None):
 
 
 def gen_noise(batch_size, noise_shape):
-    #input noise to gen seems to be very important!
+    #input noise for the generator should follow a probability distribution, like in this case, the normal distributon.
     return np.random.normal(0, 1, size=(batch_size,)+noise_shape)
 
 
 def generate_images(generator, save_dir):
     
     noise = gen_noise(batch_size,noise_shape)
-    #using noise produced by np.random.uniform - the generator seems to produce same image for ANY noise - 
-    #but those images (even though they are the same) are very close to the actual image - experiment with it later.
     fake_data_X = generator.predict(noise)
     print("Displaying generated images")
     plt.figure(figsize=(4,4))
@@ -214,7 +193,7 @@ def generate_images(generator, save_dir):
         fig.axes.get_xaxis().set_visible(False)
         fig.axes.get_yaxis().set_visible(False)
     plt.tight_layout()
-    plt.savefig(save_dir+str(time.time())+"_GENERATEDimage.png",bbox_inches='tight',pad_inches=0)
+    plt.savefig(save_dir+str(time.time())+"_GENimage.png",bbox_inches='tight',pad_inches=0)
     plt.show()
 
 
@@ -226,7 +205,6 @@ def save_img_batch(img_batch,img_save_dir):
     gs1 = gridspec.GridSpec(4, 4)
     gs1.update(wspace=0, hspace=0)
     rand_indices = np.random.choice(img_batch.shape[0],16,replace=False)
-    #print(rand_indices)
     for i in range(16):
         #plt.subplot(4, 4, i+1)
         ax1 = plt.subplot(gs1[i])
@@ -270,7 +248,7 @@ generator = get_gen_normal(noise_shape)
 
 discriminator.trainable = False
 
-opt = Adam(lr=0.00015, beta_1=0.5) #same as gen
+opt = Adam(lr=0.00015, beta_1=0.5) 
 gen_inp = Input(shape=noise_shape)
 GAN_inp = generator(gen_inp)
 GAN_opt = discriminator(GAN_inp)
@@ -305,22 +283,22 @@ for step in range(num_steps):
         step_num = str(tot_step).zfill(4)
         save_img_batch(fake_data_X,img_save_dir+step_num+"_image.png")
 
-        
+    #concatenate real and fake data samples    
     data_X = np.concatenate([real_data_X,fake_data_X])
-    
+    #add noise to the label inputs
     real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
     
-    fake_data_Y = np.random.random_sample(batch_size)*0.2
     
+    fake_data_Y = np.random.random_sample(batch_size)*0.2
      
     data_Y = np.concatenate((real_data_Y,fake_data_Y))
-    
         
     discriminator.trainable = True
     generator.trainable = False
-    
-    dis_metrics_real = discriminator.train_on_batch(real_data_X,real_data_Y)   #training seperately on real
-    dis_metrics_fake = discriminator.train_on_batch(fake_data_X,fake_data_Y)   #training seperately on fake
+    #training the discriminator on real and fake data can be done together, i.e., on the data_x and data_y, OR it can be done 
+    #one by one as performed below
+    dis_metrics_real = discriminator.train_on_batch(real_data_X,real_data_Y)  
+    dis_metrics_fake = discriminator.train_on_batch(fake_data_X,fake_data_Y)   
     
     print("Disc: real loss: %f fake loss: %f" % (dis_metrics_real[0], dis_metrics_fake[0]))
     
